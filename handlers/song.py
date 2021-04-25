1b9020a0ca11e64c3ea0b7e4808e8234abfd7583
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-04-25 16:04:56 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -1,3 +1,6 @@
+#PatriciaXmusic
+#Made and copyright © Blaze
+
 from __future__ import unicode_literals
 import os
 import requests
@@ -51,7 +54,7 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
+            "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly😂lol😂."
         )
         print(str(e))
         return
@@ -61,7 +64,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @VCPlayBot **'
+        rep = '**🎵 Uploaded by PatriciaXmusic **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
